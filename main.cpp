88abc7ec863dd085232d93fb38ffea8d57767533
Óscar Pozo <alu010103652@ull.edu.es>
2021-11-19 11:06:46 +0000

    Commit inicial

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/** Universidad de La Laguna
+*   Gestión del Conocimiento. 4º curso.
+*   Práctica: Creació de un sistema de recomendaciones.
+*   
+*   Realizado por: Óscar Ignacio Pozo Fernández
+*   Correo: alu010103526@ull.edu.es
+*/
+
+main() {}
